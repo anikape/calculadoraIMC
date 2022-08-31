7b14e8c249e44d2a9cfdb9fba85c17cafaee26cd
anikape <ana.br@hotmail.com>
2022-08-31 12:36:18 -0300

    alterações

--- imc.js
+++ imc.js
@@ -7,22 +7,63 @@ function calculate() {
 
     var classificacao = '';
 
+    var imagem = '';
+
+    var resultado = imc.toFixed(1);
+
+
+    //Calculo do IMC
     if (imc < 16) {
-        classificacao = 'Valor inválido';
+        classificacao = ' ';
+        document.getElementById("valorInvalido").style.display = "block"; // a imagem aparecerá
+        document.getElementById("textoValue").innerHTML = "Olá! <br><br> Valor inválido. Por favor, verifique as informações iseridas." //mensagem que irá aparecer
+
     } else if (imc <= 16.9) {
-        classificacao = 'Muito abaixo do peso';
+        classificacao = 'muito abaixo do peso';
+        document.getElementById("muitoAbaixo").style.display = "block";
+        document.getElementById("muitoBaixo").innerHTML = "Olá! <br> Você está muito abaixo do peso. Que tal procurar a ajuda de um nutricionista e preparar aquela alimentação legal para chegar ao peso ideal"
+
     } else if (imc <= 18.4) {
-        classificacao = 'Abaixo do peso';
+        classificacao = 'abaixo do peso';
+        document.getElementById("abaixoDoPeso").style.display = "block";
+        document.getElementById("abaixoPeso").innerHTML = "Olá! Você está abaixo do peso. Mas com a ajuda de um nutricionista podemos chegar ao peso ideal. "
+
     } else if (imc <= 24.9) {
-        classificacao = 'Peso Normal.';
+        classificacao = 'com seu peso Normal.';
+        document.getElementById("pesoNormal").style.display = "block";
+        document.getElementById("pesoNormal").innerHTML = "Olá! Parabéns. Seu peso está normal. Continue cuidando da sua saúde."
+
     } else if (imc <= 29.9) {
-        classificacao = 'Acima do Peso.';
+        classificacao = 'acima do Peso.';
+        document.getElementById("acimaDoPeso").style.display = "block";
+        document.getElementById("pesoAcima").innerHTML = "Olá! Você está acima do peso. Mas sem desespero, ok?! Exercíos físicos e uma alimentação saudável são os aliados."
+
     } else if (imc <= 34.9) {
-        classificacao = 'Obesidade grau I.';
+        classificacao = 'com Obesidade grau I.';
+        document.getElementById("ob1").style.display = "block";
+        document.getElementById("obeso1").innerHTML = "Atenção! Você está com obesidade grau 1. Que tal iniciar um checkup?"
+
     } else if (imc <= 40) {
-        classificacao = 'Obesidade grau II.';
+        classificacao = 'com Obesidade grau II.';
+        document.getElementById("ob2").style.display = "block";
+        document.getElementById("obeso2").innerHTML = "Atenção! Você está com obesidade grau 2. Que tal iniciar um checkup? "
+
     } else if (imc > 40) {
-        classificacao = 'Obesidade grau III.';
+        classificacao = 'com Obesidade grau III.';
+        document.getElementById("ob3").style.display = "block";
+        document.getElementById("obeso3").innerHTML = "Atenção! Você está com obesidade grau 3. Que tal iniciar um checkup? Cuide-se!"
+
     }
-    document.getElementById("text_area").innerHTML = classificacao
+
+
+    document.getElementById("text_area").innerHTML = resultado
+    document.getElementById("valorInvalido").innerHTML = imagem
+
 }
+
+
+
+
+
+
+// alterancia de imagens
